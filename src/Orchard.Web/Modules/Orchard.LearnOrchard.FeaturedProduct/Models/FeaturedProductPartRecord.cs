e8c3a4d262316db084bc22eea4f4b2871eb69e7e
Peter Morlion <peter.morlion@gmail.com>
2016-07-11 10:58:36 +0200

    Remove tutorial module

--- src/Orchard.Web/Modules/Orchard.LearnOrchard.FeaturedProduct/Models/FeaturedProductPartRecord.cs
+++
@@ -1,7 +0,0 @@
-﻿using Orchard.ContentManagement.Records;
-
-namespace Orchard.LearnOrchard.FeaturedProduct.Models {
-    public class FeaturedProductPartRecord : ContentPartRecord {
-        public virtual bool IsOnSale { get; set; }
-    }
-}
