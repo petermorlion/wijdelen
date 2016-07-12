fcbd169278055ef8a78a8c1a1225f7a5269d516e
Peter Morlion <peter.morlion@gmail.com>
2016-07-12 16:41:37 +0200

    Add OneTimeLoginPart and record

---
+++ src/Orchard.Web/Modules/WijDelen.UserImport/Models/OneTimeLoginPartRecord.cs
@@ -0,0 +1,8 @@
+﻿using Orchard.ContentManagement.Records;
+
+namespace WijDelen.UserImport.Models {
+    public class OneTimeLoginPartRecord : ContentPartRecord {
+        public virtual int UserId { get; set; }
+        public virtual string Token { get; set; }
+    }
+}
