042a5a400b07039cd85d9c8114d2cbc768c0586b
Peter Morlion <peter.morlion@gmail.com>
2016-08-12 12:59:34 +0200

    Add incomplete GroupService

---
+++ src/Orchard.Web/Modules/WijDelen.UserImport/Services/GroupService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Orchard.ContentManagement;
+using Orchard.Security;
+using WijDelen.UserImport.Models;
+
+namespace WijDelen.UserImport.Services {
+    public class GroupService : IGroupService {
+        private readonly IContentManager _contentManager;
+
+        public GroupService(IContentManager contentManager) {
+            _contentManager = contentManager;
+        }
+
+        public void AddUsersToGroup(string groupName, IEnumerable<IUser> users) {
+            var group = _contentManager.New("Group");
+            group.As<NamePart>().Name = groupName;
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/WijDelen.UserImport/Services/IGroupService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Orchard;
+using Orchard.Security;
+
+namespace WijDelen.UserImport.Services {
+    public interface IGroupService : IDependency {
+        void AddUsersToGroup(string groupName, IEnumerable<IUser> users);
+    }
+}
