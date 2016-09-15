7d42d0112bfc1925f2e22c053ec3540514aac705
Peter Morlion <peter.morlion@gmail.com>
2016-09-15 11:40:28 +0200

    Basic posting of data

--- src/Orchard.Web/Modules/WijDelen.ObjectSharing/Controllers/ObjectRequestController.cs
+++ src/Orchard.Web/Modules/WijDelen.ObjectSharing/Controllers/ObjectRequestController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Web.Mvc;
 using Orchard.Themes;
+using WijDelen.ObjectSharing.ViewModels;
 
 namespace WijDelen.ObjectSharing.Controllers {
     [Themed]
     public class ObjectRequestController : Controller {
         public ActionResult New() {
-            return View();
+            return View(new NewObjectRequestViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult New(NewObjectRequestViewModel viewModel) {
+            return View(viewModel);
         }
     }
 }
