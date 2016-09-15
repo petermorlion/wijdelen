e385696c24cc370b36a037527891878c217fbc39
Peter Morlion <peter.morlion@gmail.com>
2016-09-15 11:34:44 +0200

    Page for requesting an object

---
+++ src/Orchard.Web/Modules/WijDelen.ObjectSharing/Controllers/ObjectRequestController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+using Orchard.Themes;
+
+namespace WijDelen.ObjectSharing.Controllers {
+    [Themed]
+    public class ObjectRequestController : Controller {
+        public ActionResult New() {
+            return View();
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/WijDelen.ObjectSharing/ViewModels/NewObjectRequestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WijDelen.ObjectSharing.ViewModels {
+    public class NewObjectRequestViewModel {
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ExtraInfo { get; set; }
+    }
+}
