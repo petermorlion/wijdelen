906c975349c97b909284a4e0b28f111e4d8e7382
Peter Morlion <peter.morlion@gmail.com>
2016-09-15 11:55:26 +0200

    Validate user input

--- src/Orchard.Web/Modules/WijDelen.ObjectSharing/Controllers/ObjectRequestController.cs
+++ src/Orchard.Web/Modules/WijDelen.ObjectSharing/Controllers/ObjectRequestController.cs
@@ -1,17 +1,34 @@
 ﻿using System.Web.Mvc;
+using Orchard.Localization;
+using Orchard.Mvc.Extensions;
 using Orchard.Themes;
 using WijDelen.ObjectSharing.ViewModels;
 
 namespace WijDelen.ObjectSharing.Controllers {
     [Themed]
     public class ObjectRequestController : Controller {
+        public ObjectRequestController() {
+            T = NullLocalizer.Instance;
+        }
+
         public ActionResult New() {
             return View(new NewObjectRequestViewModel());
         }
 
         [HttpPost]
         public ActionResult New(NewObjectRequestViewModel viewModel) {
+            if (string.IsNullOrWhiteSpace(viewModel.Description)) {
+                ModelState.AddModelError("Description", T("Please provide a description of the item you need."));
+            }
+
+            if (string.IsNullOrWhiteSpace(viewModel.ExtraInfo))
+            {
+                ModelState.AddModelError("ExtraInfo", T("Please provide some extra info."));
+            }
+
             return View(viewModel);
         }
+
+        public Localizer T { get; set; }
     }
 }
---
+++ src/WijDelen.ObjectSharing.Tests/Controllers/ObjectRequestControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using FluentAssertions;
+using NUnit.Framework;
+using Orchard;
+using Orchard.Localization;
+using WijDelen.ObjectSharing.Controllers;
+using WijDelen.ObjectSharing.ViewModels;
+
+namespace WijDelen.ObjectSharing.Tests.Controllers {
+    [TestFixture]
+    public class ObjectRequestControllerTests {
+        /// <summary>
+        /// Verifies that T can be set (not having a setter will not cause a compile-time exception, but it will cause a runtime exception.
+        /// </summary>
+        [Test]
+        public void TestT()
+        {
+            var controller = new ObjectRequestController();
+            var localizer = NullLocalizer.Instance;
+
+            controller.T = localizer;
+
+            Assert.AreEqual(localizer, controller.T);
+        }
+
+        [Test]
+        public void ShouldValidateNewObjectRequest() {
+            var controller = new ObjectRequestController();
+            var viewModel = new NewObjectRequestViewModel();
+
+            var viewResult = controller.New(viewModel);
+
+            ((ViewResult) viewResult).ViewData.ModelState["Description"].Errors.Single().ErrorMessage.Should().Be("Please provide a description of the item you need.");
+            ((ViewResult) viewResult).ViewData.ModelState["ExtraInfo"].Errors.Single().ErrorMessage.Should().Be("Please provide some extra info.");
+        }
+    }
+}
