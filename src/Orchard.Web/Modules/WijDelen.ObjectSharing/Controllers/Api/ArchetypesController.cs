1160974da8373fefeb4951357653f5b89b542a6c
Peter Morlion <peter.morlion@gmail.com>
2016-09-29 21:55:52 +0200

    Add ApiController to get archetypes

---
+++ src/Orchard.Web/Modules/WijDelen.ObjectSharing/Controllers/Api/ArchetypesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Orchard.Data;
+using WijDelen.ObjectSharing.Models;
+
+namespace WijDelen.ObjectSharing.Controllers.Api {
+    public class ArchetypesController : ApiController {
+        private readonly IRepository<ArchetypeRecord> _archetypeRepository;
+        private readonly IRepository<ArchetypedSynonymRecord> _synonymsRepository;
+
+        public ArchetypesController(IRepository<ArchetypeRecord> archetypeRepository, IRepository<ArchetypedSynonymRecord> synonymsRepository) {
+            _archetypeRepository = archetypeRepository;
+            _synonymsRepository = synonymsRepository;
+        }
+        
+        public IHttpActionResult Get(string input) {
+            var exactMatch = _archetypeRepository.Fetch(x => string.Equals(x.Name, input, StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+            if (exactMatch.Any()) {
+                var result = exactMatch.Select(x => x.Name).ToList();
+                return Ok(result);
+            }
+
+            var itemsList = new List<string>{
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                input
+            };
+
+            return Ok(itemsList);
+        }
+    }
+}
---
+++ src/WijDelen.ObjectSharing.Tests/Controllers/Api/ArchetypesControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Http.Results;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Orchard.Data;
+using WijDelen.ObjectSharing.Controllers.Api;
+using WijDelen.ObjectSharing.Models;
+using WijDelen.ObjectSharing.Tests.Fakes;
+
+namespace WijDelen.ObjectSharing.Tests.Controllers.Api {
+    [TestFixture]
+    public class ArchetypesControllerTests {
+        [Test]
+        public void GetShouldReturnExactMatch()
+        {
+            var records = new[] {
+                new ArchetypeRecord { Name = "Sneakers" },
+                new ArchetypeRecord { Name = "Flaming Moe" }
+            };
+
+            var repositoryMock = new Mock<IRepository<ArchetypeRecord>>();
+            repositoryMock.SetRecords(records);
+
+            var controller = new ArchetypesController(repositoryMock.Object, null);
+
+            var result = (OkNegotiatedContentResult<List<string>>)controller.Get("sneakers");
+
+            result.Content.ShouldBeEquivalentTo(new List<string> { "Sneakers" });
+        }
+    }
+}
