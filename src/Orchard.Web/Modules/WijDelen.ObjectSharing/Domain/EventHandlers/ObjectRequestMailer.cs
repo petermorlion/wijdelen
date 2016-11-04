f63990ea82148b5b8c3a966dc82d1b0e13aa1917
Peter Morlion <peter.morlion@gmail.com>
2016-11-04 21:54:05 +0100

    Create new mail when object requested

--- src/Orchard.Web/Modules/WijDelen.ObjectSharing/Domain/EventHandlers/ObjectRequestMailer.cs
+++ src/Orchard.Web/Modules/WijDelen.ObjectSharing/Domain/EventHandlers/ObjectRequestMailer.cs
@@ -1,10 +1,27 @@
-﻿using WijDelen.ObjectSharing.Domain.Events;
+﻿using System;
+using WijDelen.ObjectSharing.Domain.Entities;
+using WijDelen.ObjectSharing.Domain.Events;
+using WijDelen.ObjectSharing.Domain.EventSourcing;
 using WijDelen.ObjectSharing.Domain.Messaging;
 
 namespace WijDelen.ObjectSharing.Domain.EventHandlers {
     public class ObjectRequestMailer : IEventHandler<ObjectRequested>
     {
-        public void Handle(ObjectRequested e) {
+        private readonly IEventSourcedRepository<ObjectRequestMail> _repository;
+
+        public ObjectRequestMailer(IEventSourcedRepository<ObjectRequestMail> repository) {
+            _repository = repository;
+        }
+
+        public void Handle(ObjectRequested objectRequested) {
+            var objectRequestMail = new ObjectRequestMail(
+                Guid.NewGuid(), 
+                objectRequested.UserId, 
+                new string[0], 
+                objectRequested.Description, 
+                objectRequested.ExtraInfo);
+
+            _repository.Save(objectRequestMail, Guid.NewGuid().ToString());
         }
     }
 }
---
+++ src/WijDelen.ObjectSharing.Tests/Domain/EventHandlers/ObjectRequestMailerTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using WijDelen.ObjectSharing.Domain.Entities;
+using WijDelen.ObjectSharing.Domain.EventHandlers;
+using WijDelen.ObjectSharing.Domain.Events;
+using WijDelen.ObjectSharing.Domain.EventSourcing;
+
+namespace WijDelen.ObjectSharing.Tests.Domain.EventHandlers {
+    [TestFixture]
+    public class ObjectRequestMailerTests {
+        [Test]
+        public void WhenObjectIsRequested() {
+            var objectRequested = new ObjectRequested {
+                UserId = 3,
+                Description = "Sneakers",
+                ExtraInfo = "For sneaking"
+            };
+
+            ObjectRequestMail persistedMail = null;
+
+            var repositoryMock = new Mock<IEventSourcedRepository<ObjectRequestMail>>();
+            repositoryMock
+                .Setup(x => x.Save(It.IsAny<ObjectRequestMail>(), It.IsAny<string>()))
+                .Callback((ObjectRequestMail mail, string correlationId) => { persistedMail = mail; });
+
+
+            var handler = new ObjectRequestMailer(repositoryMock.Object);
+
+            handler.Handle(objectRequested);
+
+            persistedMail.Should().NotBeNull();
+            persistedMail.UserId.Should().Be(3);
+            persistedMail.Description.Should().Be("Sneakers");
+            persistedMail.ExtraInfo.Should().Be("For sneaking");
+        }
+    }
+}
