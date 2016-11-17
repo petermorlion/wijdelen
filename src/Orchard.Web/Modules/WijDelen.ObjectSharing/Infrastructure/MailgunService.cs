a2a11294081ba4a1f80c611aea4de298eb229e26
Peter Morlion <peter.morlion@gmail.com>
2016-11-17 08:29:23 +0100

    Implement mailgunservice to send object request mail

--- src/Orchard.Web/Modules/WijDelen.ObjectSharing/Infrastructure/MailgunService.cs
+++ src/Orchard.Web/Modules/WijDelen.ObjectSharing/Infrastructure/MailgunService.cs
@@ -1,4 +1,8 @@
-﻿using Orchard.Localization;
+﻿using System;
+using System.Collections.Generic;
+using Orchard.Localization;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace WijDelen.ObjectSharing.Infrastructure {
     public class MailgunService : IMailService {
@@ -9,7 +13,37 @@ namespace WijDelen.ObjectSharing.Infrastructure {
 
         public Localizer T { get; set; }
         public void SendObjectRequestMail(string description, string extraInfo, params string[] emailAddresses) {
-            throw new System.NotImplementedException();
+            var client = new RestClient
+            {
+                BaseUrl = new Uri("https://api.mailgun.net/v3"),
+                Authenticator = new HttpBasicAuthenticator("api", "key-9b8b2053d33de2583bfd3afb604dd820")
+            };
+
+            var request = new RestRequest();
+            request.AddParameter("domain", "sandboxaa07be2124b6407f8b84a25c232b739c.mailgun.org", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Mailgun Sandbox <postmaster@sandboxaa07be2124b6407f8b84a25c232b739c.mailgun.org>");
+
+            var recipientVariables = new List<string>();
+
+            foreach (var emailAddress in emailAddresses) {
+                var yesLink = "";
+                var notNowLink = "";
+                var noLink = "";
+
+                request.AddParameter("to", $"{emailAddress}");
+                recipientVariables.Add($"\"{emailAddress}\": {{\"yesLink\":\"{yesLink}\", \"notNowLink\":\"{notNowLink}\", \"noLink\":\"{noLink}\"}}");
+            }
+
+            request.AddParameter("recipient-variables", $"{{{string.Join(",", recipientVariables)}}}");
+
+            request.AddParameter("subject", T("Do you have a {0}?", description).ToString());
+            request.AddParameter("text", T("object-request-mail-text", description, extraInfo).ToString());
+            request.AddParameter("html", T("object-request-mail-html", description, extraInfo).ToString());
+
+            request.Method = Method.POST;
+
+            client.Execute(request);
         }
     }
 }
