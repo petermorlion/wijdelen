ebfe821a977fc4ffe9b7151c3e9a676b30c813ef
Peter Morlion <peter.morlion@gmail.com>
2016-11-18 19:56:11 +0100

    ConfirmObjectRequest command

---
+++ src/Orchard.Web/Modules/WijDelen.ObjectSharing/Domain/Commands/ConfirmObjectRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WijDelen.ObjectSharing.Domain.Messaging;
+
+namespace WijDelen.ObjectSharing.Domain.Commands {
+    public class ConfirmObjectRequest : ICommand {
+        public ConfirmObjectRequest(int confirmingUserId) {
+            Id = Guid.NewGuid();
+            ConfirmingUserId = confirmingUserId;
+        }
+
+        public Guid Id { get; }
+        public int ConfirmingUserId { get; }
+    }
+}
