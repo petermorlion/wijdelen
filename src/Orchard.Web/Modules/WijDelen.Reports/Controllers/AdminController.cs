1391a991749961b266a465da34ac9695cc5b65df
Peter Morlion <peter.morlion@gmail.com>
2017-01-26 16:25:16 +0100

    Fix assets pipeline

--- src/Orchard.Web/Modules/WijDelen.Reports/Controllers/AdminController.cs
+++ src/Orchard.Web/Modules/WijDelen.Reports/Controllers/AdminController.cs
@@ -1,9 +1,18 @@
 ﻿using System.Web.Mvc;
+using WijDelen.Reports.ViewModels;
 
 namespace WijDelen.Reports.Controllers {
     public class AdminController : Controller {
         public ActionResult Index() {
-            return View();
+
+
+            var viewModel = new DashboardViewModel {
+                TotalUsers = 39,
+                TotalGroups = 12,
+                TotalObjectRequests = 885
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ src/Orchard.Web/Modules/WijDelen.Reports/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WijDelen.Reports.ViewModels {
+    public class DashboardViewModel {
+        public int TotalUsers { get; set; }
+        public int TotalGroups { get; set; }
+        public int TotalObjectRequests { get; set; }
+    }
+}
