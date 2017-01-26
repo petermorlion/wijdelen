bcaec8e33e937942e2ce5d235981f1d71853c9de
Peter Morlion <peter.morlion@gmail.com>
2017-01-26 14:38:06 +0100

    Add empty reports page

---
+++ src/Orchard.Web/Modules/WijDelen.Reports/AdminMenu.cs
@@ -0,0 +1,31 @@
+﻿using Orchard.Localization;
+using Orchard.Security;
+using Orchard.UI.Navigation;
+
+namespace WijDelen.Reports {
+    public class AdminMenu : INavigationProvider
+    {
+        public AdminMenu()
+        {
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        public string MenuName
+        {
+            get { return "admin"; }
+        }
+
+        public void GetNavigation(NavigationBuilder builder)
+        {
+            builder.Add(item => item
+                .Caption(T("Peergroups"))
+                .Add(subItem => subItem
+                    .Caption(T("Reports"))
+                    .Position("5")
+                    .Action("Index", "Admin", new { area = "WijDelen.Reports" })
+                    .Permission(StandardPermissions.SiteOwner)));
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/WijDelen.Reports/Controllers/AdminController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace WijDelen.Reports.Controllers {
+    public class AdminController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
