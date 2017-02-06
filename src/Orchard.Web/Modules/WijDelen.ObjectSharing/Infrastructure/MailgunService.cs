e30495942f622ba4b9dbaee19783af5a0f0302c2
Peter Morlion <peter.morlion@gmail.com>
2017-02-06 21:31:16 +0100

    Refactor duplicate code for calling REST api of Mailgun

--- src/Orchard.Web/Modules/WijDelen.ObjectSharing/Infrastructure/MailgunService.cs
+++ src/Orchard.Web/Modules/WijDelen.ObjectSharing/Infrastructure/MailgunService.cs
@@ -6,35 +6,28 @@ using RestSharp;
 using RestSharp.Authenticators;
 using WijDelen.ObjectSharing.Domain.Entities;
 using WijDelen.ObjectSharing.Domain.ValueTypes;
+using WijDelen.ObjectSharing.Infrastructure.Factories;
 
 namespace WijDelen.ObjectSharing.Infrastructure {
     public class MailgunService : IMailService {
         private readonly IOrchardServices _orchardServices;
+        private readonly IMailgunClient _mailgunClient;
 
-        public MailgunService(IOrchardServices orchardServices) {
+        public MailgunService(IOrchardServices orchardServices, IMailgunClient mailgunClient) {
             _orchardServices = orchardServices;
+            _mailgunClient = mailgunClient;
 
             T = NullLocalizer.Instance;
         }
 
         public Localizer T { get; set; }
         public void SendObjectRequestMail(string requestingUserName, string groupName, Guid objectRequestId, string description, string extraInfo, ObjectRequestMail objectRequestMail, params UserEmail[] userEmails) {
-            var client = new RestClient
-            {
-                BaseUrl = new Uri("https://api.mailgun.net/v3"),
-                Authenticator = new HttpBasicAuthenticator("api", "key-9b8b2053d33de2583bfd3afb604dd820")
-            };
-
-            var request = new RestRequest();
-            request.AddParameter("domain", "sandboxaa07be2124b6407f8b84a25c232b739c.mailgun.org", ParameterType.UrlSegment);
-            request.Resource = "{domain}/messages";
-            request.AddParameter("from", "Mailgun Sandbox <postmaster@sandboxaa07be2124b6407f8b84a25c232b739c.mailgun.org>");
-
+            var recipients = new List<string>();
             foreach (var userEmail in userEmails) {
-                request.AddParameter("to", $"{userEmail.Email}");
+                recipients.Add(userEmail.Email);
             }
 
-            request.AddParameter("subject", T("Do you have a {0}?", description).ToString());
+            var subject = T("Do you have (a) {0}?", description).ToString();
 
             var siteUrl = _orchardServices.WorkContext.CurrentSite.BaseUrl;
             var yesLink = siteUrl + "/WijDelen.ObjectSharing/ObjectRequestResponse/Confirm/" + objectRequestId;
@@ -42,39 +35,23 @@ namespace WijDelen.ObjectSharing.Infrastructure {
             var noLink = siteUrl + "/WijDelen.ObjectSharing/ObjectRequestResponse/Deny/" + objectRequestId;
 
             var htmlBody = T("object-request-mail-html", requestingUserName, groupName, description, extraInfo, yesLink, notNowLink, noLink).ToString();
-            request.AddParameter("text", T("object-request-mail-text", requestingUserName, groupName, description, extraInfo, yesLink, notNowLink, noLink).ToString());
-            request.AddParameter("html", htmlBody);
+            var textBody = T("object-request-mail-text", requestingUserName, groupName, description, extraInfo, yesLink, notNowLink, noLink).ToString();
 
-            request.Method = Method.POST;
-
-            client.Execute(request);
+            _mailgunClient.Send(recipients, "", subject, textBody, htmlBody);
 
             objectRequestMail.MarkAsSent(userEmails, htmlBody);
         }
 
         public void SendChatMessageAddedMail(string fromUserName, string toUserName, string description, string toEmailAddress, Guid chatId, string message) {
-            var client = new RestClient
-            {
-                BaseUrl = new Uri("https://api.mailgun.net/v3"),
-                Authenticator = new HttpBasicAuthenticator("api", "key-9b8b2053d33de2583bfd3afb604dd820")
-            };
-
-            var request = new RestRequest();
-            request.AddParameter("domain", "sandboxaa07be2124b6407f8b84a25c232b739c.mailgun.org", ParameterType.UrlSegment);
-            request.Resource = "{domain}/messages";
-            request.AddParameter("from", "Mailgun Sandbox <postmaster@sandboxaa07be2124b6407f8b84a25c232b739c.mailgun.org>");
-            request.AddParameter("to", $"{toEmailAddress}");
-            request.AddParameter("subject", T("{0} reacted on your request for a {1}.", fromUserName, description).ToString());
+            var subject = T("{0} reacted on your request for a {1}.", fromUserName, description).ToString();
+            var to = new[] {toEmailAddress};
 
             var chatUrl = _orchardServices.WorkContext.CurrentSite.BaseUrl + "/WijDelen.ObjectSharing/Chat/Index/" + chatId;
             
             var htmlBody = T("chat-message-added-mail-html", fromUserName, description, message, chatUrl).ToString();
-            request.AddParameter("text", T("chat-message-added-mail-text", fromUserName, description, message, chatUrl).ToString());
-            request.AddParameter("html", htmlBody);
+            var textBody = T("chat-message-added-mail-text", fromUserName, description, message, chatUrl).ToString();
 
-            request.Method = Method.POST;
-
-            client.Execute(request);
+            _mailgunClient.Send(to, "", subject, textBody, htmlBody);
         }
     }
 }
---
+++ src/WijDelen.Mailgun/MailgunClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace WijDelen.Mailgun {
+    public class MailgunClient :
+        ObjectSharing.Infrastructure.Factories.IMailgunClient,
+        UserImport.Factories.IMailgunClient {
+        /// <summary>
+        /// Creates an initial authenticated POST request for Mailgun, including the From address. Anything
+        /// else should be added (to, subject, text, html).
+        /// </summary>
+        /// <param name="recipients">A list of recipients, usually in the form "Name &lt;Email&gt;".</param>
+        /// <param name="recipientVariables">Recipient variables, as documented by Mailgun. Pass in an empty string if there are none.</param>
+        /// <param name="subject">The subject of the email.</param>
+        /// <param name="textMail">A text version of the mail.</param>
+        /// <param name="htmlMail">A html version of the mail.</param>
+        public void Send(IEnumerable<string> recipients, string recipientVariables, string subject, string textMail, string htmlMail) {
+            var client = new RestClient {
+                BaseUrl = new Uri("https://api.mailgun.net/v3"),
+                Authenticator = new HttpBasicAuthenticator("api", "key-9b8b2053d33de2583bfd3afb604dd820")
+            };
+
+            var request = new RestRequest();
+            request.AddParameter("domain", "sandboxaa07be2124b6407f8b84a25c232b739c.mailgun.org", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Mailgun Sandbox <postmaster@sandboxaa07be2124b6407f8b84a25c232b739c.mailgun.org>");
+            request.AddParameter("subject", subject);
+            request.AddParameter("text", textMail);
+            request.AddParameter("html", htmlMail);
+
+            foreach (var recipient in recipients) {
+                request.AddParameter("to", recipient);
+            }
+
+            if (!string.IsNullOrEmpty(recipientVariables)) {
+                request.AddParameter("recipient-variables", recipientVariables);
+            }
+
+            request.Method = Method.POST;
+
+            client.Execute(request);
+        }
+    }
+}
