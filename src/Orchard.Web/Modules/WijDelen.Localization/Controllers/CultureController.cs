830075a3fa75523f20ee8ccee9b6ac674411a95b
Peter Morlion <peter.morlion@gmail.com>
2017-03-16 16:25:17 +0100

    Allow changing language
    Language/culture is stored in a cookie, using a new module (because controller in theme couldn't be called)

---
+++ src/Orchard.Web/Modules/WijDelen.Localization/Controllers/CultureController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Orchard.Localization.Providers;
+using Orchard.Mvc.Extensions;
+
+namespace WijDelen.Localization.Controllers {
+    public class CultureController : Controller {
+        private readonly ICultureStorageProvider _cultureStorageProvider;
+
+        public CultureController(ICultureStorageProvider cultureStorageProvider) {
+            _cultureStorageProvider = cultureStorageProvider;
+        }
+
+        public ActionResult ChangeCulture(string culture, string returnUrl) {
+            _cultureStorageProvider.SetCulture(culture);
+
+            return this.RedirectLocal(returnUrl);
+        }
+    }
+}
