a4b0b91080069cbafdf45c5552dd15986de2ac4d
Peter Morlion <peter.morlion@gmail.com>
2017-03-17 14:47:17 +0100

    Create copy of AccountController.LostPassword methods
    This will serve as a controller to register the user when he/she clicks on the link in the invitation mail

---
+++ src/Orchard.Web/Modules/WijDelen.UserImport/Controllers/RegisterController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Web.Mvc;
+using Orchard.Mvc.Extensions;
+using Orchard.Security;
+using Orchard.Themes;
+using Orchard.Users.Services;
+using Orchard.Localization;
+using Orchard.Users.Events;
+
+namespace WijDelen.UserImport.Controllers {
+    /// <summary>
+    /// A copy of AccountController.LostPassword because we want to control the logic and the UI when the user clicks on the link in the invitation mail.
+    /// </summary>
+    [Themed]
+    public class RegisterController : Controller {
+        private readonly IUserService _userService;
+        private readonly IMembershipService _membershipService;
+        private readonly IUserEventHandler _userEventHandler;
+
+        public RegisterController(IUserService userService, IMembershipService membershipService, IUserEventHandler userEventHandler) {
+            _userService = userService;
+            _membershipService = membershipService;
+            _userEventHandler = userEventHandler;
+        }
+
+        [AlwaysAccessible]
+        public ActionResult Index(string nonce)
+        {
+            if (_userService.ValidateLostPassword(nonce) == null)
+            {
+                return RedirectToAction("LogOn");
+            }
+
+            ViewData["PasswordLength"] = MinPasswordLength;
+
+            return View();
+        }
+
+        [HttpPost]
+        [AlwaysAccessible]
+        [ValidateInput(false)]
+        public ActionResult Index(string nonce, string newPassword, string confirmPassword)
+        {
+            IUser user;
+            if ((user = _userService.ValidateLostPassword(nonce)) == null)
+            {
+                return Redirect("~/");
+            }
+
+            ViewData["PasswordLength"] = MinPasswordLength;
+
+            if (newPassword == null || newPassword.Length < MinPasswordLength)
+            {
+                ModelState.AddModelError("newPassword", T("You must specify a new password of {0} or more characters.", MinPasswordLength));
+            }
+
+            if (!string.Equals(newPassword, confirmPassword, StringComparison.Ordinal))
+            {
+                ModelState.AddModelError("_FORM", T("The new password and confirmation password do not match."));
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _membershipService.SetPassword(user, newPassword);
+
+            _userEventHandler.ChangedPassword(user);
+
+            return RedirectToAction("ChangePasswordSuccess");
+        }
+
+        public Localizer T { get; set; }
+
+        int MinPasswordLength { 
+            get {
+                return _membershipService.GetSettings().MinRequiredPasswordLength;
+            }
+        }
+    }
+}
---
+++ src/Orchard.Web/Modules/WijDelen.UserImport/WijDelen.UserImport.Tests/Controllers/RegisterControllerTests.cs
@@ -0,0 +1,131 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using Orchard.Localization;
+using Orchard.Security;
+using Orchard.Users.Events;
+using Orchard.Users.Services;
+using WijDelen.UserImport.Controllers;
+
+namespace WijDelen.UserImport.Tests.Controllers {
+    [TestFixture]
+    public class RegisterControllerTests {
+        /// <summary>
+        /// Verifies that T can be set (not having a setter will not cause a compile-time exception, but it will cause a runtime exception.
+        /// </summary>
+        [Test]
+        public void TestT()
+        {
+            var controller = new RegisterController(Mock.Of<IUserService>(), Mock.Of<IMembershipService>(), Mock.Of<IUserEventHandler>());
+            var localizer = NullLocalizer.Instance;
+
+            controller.T = localizer;
+
+            Assert.AreEqual(localizer, controller.T);
+        }
+
+        [Test]
+        public void TestGetWithoutNonce() {
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(x => x.ValidateLostPassword("nonce")).Returns((IUser)null); 
+            var controller = new RegisterController(userServiceMock.Object, Mock.Of<IMembershipService>(), Mock.Of<IUserEventHandler>());
+
+            var result = controller.Index("nonce");
+
+            Assert.IsInstanceOf<RedirectToRouteResult>(result);
+            Assert.AreEqual("LogOn", ((RedirectToRouteResult)result).RouteValues["action"]);
+        }
+
+        [Test]
+        public void TestGetWithNonce() {
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(x => x.ValidateLostPassword("nonce")).Returns(Mock.Of<IUser>());
+
+            var membershipServiceMock = new Mock<IMembershipService>();
+            membershipServiceMock.Setup(x => x.GetSettings()).Returns(new MembershipSettings { MinRequiredPasswordLength = 7 });
+
+            var controller = new RegisterController(userServiceMock.Object, membershipServiceMock.Object, Mock.Of<IUserEventHandler>());
+
+            var result = controller.Index("nonce");
+
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.AreEqual(7, ((ViewResult)result).ViewData["PasswordLength"]);
+        }
+
+        [Test]
+        public void TestPostWithoutNonce()
+        {
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(x => x.ValidateLostPassword("nonce")).Returns((IUser)null);
+            var controller = new RegisterController(userServiceMock.Object, Mock.Of<IMembershipService>(), Mock.Of<IUserEventHandler>());
+
+            var result = controller.Index("nonce", "pwd", "pwd");
+
+            Assert.IsInstanceOf<RedirectResult>(result);
+            Assert.AreEqual("~/", ((RedirectResult)result).Url);
+        }
+
+        [Test]
+        public void TestPostWithPasswordTooShort()
+        {
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(x => x.ValidateLostPassword("nonce")).Returns(Mock.Of<IUser>());
+
+            var membershipServiceMock = new Mock<IMembershipService>();
+            membershipServiceMock.Setup(x => x.GetSettings()).Returns(new MembershipSettings { MinRequiredPasswordLength = 7 });
+
+            var controller = new RegisterController(userServiceMock.Object, membershipServiceMock.Object, Mock.Of<IUserEventHandler>());
+            controller.T = NullLocalizer.Instance;
+
+            var result = controller.Index("nonce", "pwd", "pwd");
+
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.AreEqual("", ((ViewResult)result).ViewName);
+            Assert.AreEqual("You must specify a new password of 7 or more characters.", ((ViewResult)result).ViewData.ModelState["newPassword"].Errors.Single().ErrorMessage);
+        }
+
+        [Test]
+        public void TestPostWithPasswordsNotMatching()
+        {
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(x => x.ValidateLostPassword("nonce")).Returns(Mock.Of<IUser>());
+
+            var membershipServiceMock = new Mock<IMembershipService>();
+            membershipServiceMock.Setup(x => x.GetSettings()).Returns(new MembershipSettings { MinRequiredPasswordLength = 7 });
+
+            var controller = new RegisterController(userServiceMock.Object, membershipServiceMock.Object, Mock.Of<IUserEventHandler>());
+            controller.T = NullLocalizer.Instance;
+
+            var result = controller.Index("nonce", "password1", "password2");
+
+            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.AreEqual("", ((ViewResult)result).ViewName);
+            Assert.AreEqual("The new password and confirmation password do not match.", ((ViewResult)result).ViewData.ModelState["_FORM"].Errors.Single().ErrorMessage);
+        }
+
+        [Test]
+        public void TestSuccessfulPost()
+        {
+            var userServiceMock = new Mock<IUserService>();
+            var user = Mock.Of<IUser>();
+            userServiceMock.Setup(x => x.ValidateLostPassword("nonce")).Returns(user);
+
+            var membershipServiceMock = new Mock<IMembershipService>();
+            membershipServiceMock.Setup(x => x.GetSettings()).Returns(new MembershipSettings { MinRequiredPasswordLength = 7 });
+
+            var userEventHandlerMock = new Mock<IUserEventHandler>();
+
+            var controller = new RegisterController(userServiceMock.Object, membershipServiceMock.Object, userEventHandlerMock.Object);
+            controller.T = NullLocalizer.Instance;
+
+            var result = controller.Index("nonce", "password1", "password1");
+
+
+            membershipServiceMock.Verify(x => x.SetPassword(user, "password1"));
+            userEventHandlerMock.Verify(x => x.ChangedPassword(user));
+            Assert.IsInstanceOf<RedirectToRouteResult>(result);
+            Assert.AreEqual("ChangePasswordSuccess", ((RedirectToRouteResult)result).RouteValues["action"]);
+        }
+    }
+}
