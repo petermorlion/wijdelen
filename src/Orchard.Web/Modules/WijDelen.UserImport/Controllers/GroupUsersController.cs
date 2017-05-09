0c3f0eccaa29d570900cf8c9c31f71de4ffadb4c
Peter Morlion <peter.morlion@gmail.com>
2017-05-09 21:34:49 +0200

    Allow filtering by group

--- src/Orchard.Web/Modules/WijDelen.UserImport/Controllers/GroupUsersController.cs
+++ src/Orchard.Web/Modules/WijDelen.UserImport/Controllers/GroupUsersController.cs
@@ -1,26 +1,28 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard;
 using Orchard.ContentManagement;
 using Orchard.DisplayManagement;
 using Orchard.Localization;
+using Orchard.Mvc;
 using Orchard.Security;
-using Orchard.Users.Models;
-using Orchard;
 using Orchard.Settings;
 using Orchard.UI.Admin;
 using Orchard.UI.Navigation;
+using Orchard.Users.Models;
+using Orchard.Users.ViewModels;
+using WijDelen.UserImport.Models;
 using WijDelen.UserImport.Services;
 using WijDelen.UserImport.ViewModels;
 
 namespace WijDelen.UserImport.Controllers {
     [Admin]
     public class GroupUsersController : Controller {
+        private readonly IGroupService _groupService;
         private readonly IOrchardServices _orchardServices;
         private readonly ISiteService _siteService;
-        private readonly IGroupService _groupService;
-
-        public Localizer T { get; set; }
-        dynamic Shape { get; set; }
 
         public GroupUsersController(IOrchardServices orchardServices, ISiteService siteService, IShapeFactory shapeFactory, IGroupService groupService) {
             _orchardServices = orchardServices;
@@ -31,18 +33,26 @@ namespace WijDelen.UserImport.Controllers {
             T = NullLocalizer.Instance;
         }
 
-        public ActionResult Index(PagerParameters pagerParameters) {
+        public Localizer T { get; set; }
+        private dynamic Shape { get; }
+
+        public ActionResult Index(PagerParameters pagerParameters, int selectedGroupId = 0) {
             if (!_orchardServices.Authorizer.Authorize(StandardPermissions.SiteOwner, T("You are not authorized to view this page.")))
                 return new HttpUnauthorizedResult();
 
             var pager = new Pager(_siteService.GetSiteSettings(), pagerParameters);
 
-            var users = _orchardServices.ContentManager.Query<UserPart, UserPartRecord>();
+            var users = _orchardServices.ContentManager.Query<UserPart, UserPartRecord>().List().Select(x => x.ContentItem).ToList();
+            
+            if (selectedGroupId > 0) {
+                users = users.Where(x => x.As<GroupMembershipPart>().Group.Id == selectedGroupId).ToList();
+            }
 
-            var pagerShape = Shape.Pager(pager).TotalItemCount(users.Count());
+            var pagerShape = Shape.Pager(pager).TotalItemCount(users.Count);
 
             var results = users
-                .Slice(pager.GetStartIndex(), pager.PageSize)
+                .Skip(pager.GetStartIndex())
+                .Take(pager.PageSize)
                 .ToList();
 
             var groups = _groupService.GetGroups().OrderBy(x => x.Name).ToList();
@@ -50,11 +60,16 @@ namespace WijDelen.UserImport.Controllers {
 
             var model = new GroupUsersIndexViewModel {
                 Users = results
-                    .Select(x => new GroupUserEntry { User = x.Record })
+                    .Select(x => new GroupUserEntry {User = x.As<UserPart>().Record})
                     .ToList(),
                 Pager = pagerShape,
                 Groups = groups
             };
+
+            var routeData = new RouteData();
+            routeData.Values.Add("SelectedGroupId", selectedGroupId);
+
+            pagerShape.RouteData(routeData);
 
             return View(model);
         }
