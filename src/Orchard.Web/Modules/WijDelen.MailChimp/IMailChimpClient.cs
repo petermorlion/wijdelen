e5de049e1bdf5c69bde841d5fc5a2a38dbfcb494
Peter Morlion <peter.morlion@gmail.com>
2017-07-20 16:33:59 +0200

    Add logic to get mailchimp subscription status

---
+++ src/Orchard.Web/Modules/WijDelen.MailChimp/IMailChimpClient.cs
@@ -0,0 +1,10 @@
+﻿using Orchard;
+
+namespace WijDelen.MailChimp {
+    public interface IMailChimpClient : IDependency
+    {
+        bool IsSubscribed(string email);
+        void Subscribe(string email);
+        void Unsubscribe(string email);
+    }
+}
---
+++ src/Orchard.Web/Modules/WijDelen.MailChimp/MailChimpClient.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using Orchard.Logging;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace WijDelen.MailChimp {
+    public class MailChimpClient : IMailChimpClient {
+        private readonly string _apiKey;
+        private readonly Uri _apiBaseUrl;
+        private readonly string _listId;
+
+        public MailChimpClient() {
+            Logger = NullLogger.Instance;
+
+            _apiKey = "43a96c957b782a875beca1b56d9c854a-us16";
+            _apiBaseUrl = new Uri("https://us16.api.mailchimp.com/3.0/");
+
+#if DEBUG
+            _listId = "33299";
+#else
+            _listId = "11975"; 
+#endif
+        }
+
+        public ILogger Logger { get; set; }
+
+        public bool IsSubscribed(string email) {
+            var client = new RestClient {
+                BaseUrl = _apiBaseUrl,
+                Authenticator = new HttpBasicAuthenticator("api", _apiKey)
+            };
+
+            var emailHash = GetHash(email);
+
+            var request = new RestRequest();
+            request.Resource = "list/{listId}/members/{emailHash}";
+            request.AddUrlSegment("listId", _listId);
+            request.AddUrlSegment("emailHash", emailHash);
+            request.Method = Method.GET;
+            var response = client.Execute(request);
+
+            return response.StatusCode == HttpStatusCode.OK && ((dynamic) response.Content).status == "subscribed";
+        }
+
+        public void Subscribe(string email) {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe(string email) {
+            throw new NotImplementedException();
+        }
+
+        private static string GetHash(string input) {
+            using (var md5Hash = MD5.Create()) {
+                var hashBytes = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var stringBuilder = new StringBuilder();
+                foreach (var hashByte in hashBytes)
+                    stringBuilder.Append(hashByte.ToString("x2"));
+
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
