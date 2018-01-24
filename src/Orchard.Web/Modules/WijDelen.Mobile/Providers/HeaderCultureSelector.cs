956af388aac1388d6c6ff0be9b504147fcc697f7
Peter Morlion <peter.morlion@gmail.com>
2018-01-24 10:56:32 +0100

    Only use HeaderCultureSelector for the mobile app

--- src/Orchard.Web/Modules/WijDelen.Mobile/Providers/HeaderCultureSelector.cs
+++ src/Orchard.Web/Modules/WijDelen.Mobile/Providers/HeaderCultureSelector.cs
@@ -11,20 +11,26 @@ namespace WijDelen.Mobile.Providers {
         }
 
         public CultureSelectorResult GetCulture(HttpContextBase context) {
+            if (!IsAppRequest(context))
+                return null;
+
             var httpContext = _httpContextAccessor.Current();
 
             var languageHeader = httpContext?.Request.Headers["Accept-Language"];
 
-            if (languageHeader == null) {
-                return null;
-            }
+            if (languageHeader == null) return null;
 
             var culture = languageHeader;
-            if (languageHeader.Contains(",")) {
-                culture = languageHeader.Substring(0, languageHeader.IndexOf(","));
-            }
+            if (languageHeader.Contains(",")) culture = languageHeader.Substring(0, languageHeader.IndexOf(","));
 
-            return new CultureSelectorResult { Priority = 1, CultureName = culture };
+            return new CultureSelectorResult {Priority = 1, CultureName = culture};
+        }
+
+        /// <summary>
+        ///     Determines whether or not the current request comes from the mobile app.
+        /// </summary>
+        private bool IsAppRequest(HttpContextBase context) {
+            return context.Request.RequestContext.HttpContext.Request.Headers["Authorization"] != null;
         }
     }
 }
