fdc3c8ca8a701cfb0a5b0ae5b89eb653fcfcb5b1
Peter Morlion <peter.morlion@gmail.com>
2018-02-19 14:31:13 +0100

    Replace filesaver with xlsx
    Hopefuly, this will fix Safari issue

--- src/Orchard.Web/Modules/WijDelen.Reports/Scripts/ExcelExport.js
+++ src/Orchard.Web/Modules/WijDelen.Reports/Scripts/ExcelExport.js
@@ -1,27 +1,5 @@
-function s2ab(s) {
-    if (typeof ArrayBuffer !== 'undefined') {
-        var buf = new ArrayBuffer(s.length);
-        var view = new Uint8Array(buf);
-        for (var i = 0; i != s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
-        return buf;
-    } else {
-        var buf = new Array(s.length);
-        for (var i = 0; i != s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
-        return buf;
-    }
-}
-
 function exportToExcel(tableId, fileName) {
-    var reportTable = document.getElementById(tableId);
-
-    var wb = XLSX.utils.table_to_book(reportTable, { sheet: "Data" });
-    var wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'binary' });
-    var fname = fileName + '.xlsx';
-    try {
-        saveAs(new Blob([s2ab(wbout)], { type: "data:application/octet-stream" }), fname);
-    } catch (e) {
-        if (typeof console != 'undefined') console.log(e, wbout);
-    }
-
-    return wbout;
+    var elt = document.getElementById(tableId);
+    var wb = XLSX.utils.table_to_book(elt, { sheet: fileName });
+    return XLSX.writeFile(wb, fileName + '.xlsx');
 }
